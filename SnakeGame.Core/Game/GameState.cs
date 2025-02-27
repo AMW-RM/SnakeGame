ab67a4e3f5d6228b57f5d51ccd0c2f8731c666ac
Rafał Mąka <r.maka@amw-it.pl>
2025-02-27 17:14:04 +0100

    Funkconalnosc GameState

---
+++ SnakeGame.Core/Game/GameState.cs
@@ -0,0 +1,42 @@
+﻿using SnakeGame.Core.Interfaces;
+using SnakeGame.Core.Models;
+using System.Drawing;
+
+namespace SnakeGame.Core.Game;
+
+internal class GameState : IGameState
+{
+    public Snake Snake { get; private set; }
+    public Position Food { get; private set; }
+    public bool IsGameOver { get; private set; }
+    public int GameBoardWidth { get; private set; }
+    public int GameBoardHeight { get; private set; }
+
+    public GameState(int widht, int height)
+    {
+        GameBoardWidth = widht;
+        GameBoardHeight = height;
+
+        InitializationGame();
+    }
+    public void Reset()
+    {
+        InitializationGame();
+    }
+
+    public void SetFood(Position food)
+    {
+        Food = food;
+    }
+
+    public void SetGameOver()
+    {
+        IsGameOver = true;
+    }
+        private void InitializationGame()
+    {
+        Snake = new Snake(new Position(GameBoardWidth / 2, GameBoardHeight / 2), Direction.Down);
+        IsGameOver = false;
+
+    }
+    }
---
+++ SnakeGame.Core/Interfaces/IGameState.cs
@@ -0,0 +1,13 @@
+﻿using SnakeGame.Core.Models;
+
+namespace SnakeGame.Core.Interfaces;
+
+internal interface IGameState
+{
+    Snake Snake { get; }
+    Position Food { get; }
+    bool IsGameOver { get; }
+    int GameBoardWidth { get; } 
+    int GameBoardHeight { get; }
+
+}
